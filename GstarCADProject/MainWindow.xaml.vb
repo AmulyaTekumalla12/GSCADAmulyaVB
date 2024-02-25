ce1d1b28f5b31f2ae1fcb9c1cc43531e73a9359c
AmulyaTekumalla12 <161196280+AmulyaTekumalla12@users.noreply.github.com>
2024-02-25 22:56:32 +0530

    Delete GstarCADProject directory

--- GstarCADProject/MainWindow.xaml.vb
+++
@@ -1,7 +0,0 @@
-﻿Class MainWindow
-    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-        'Dim appLayers As GStarLayers = New GStarLayers()
-        'appLayers.Main()
-        addline.addlinecmd.DoIt()
-    End Sub
-End Class
